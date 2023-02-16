e716a1162b56971261e3af873f393d3bac6c71f0
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2023-02-16 18:07:17 -0300

    Test auto-updating

--- src/renderer/src/pages/auth/login.vue
+++ src/renderer/src/pages/auth/login.vue
@@ -20,7 +20,7 @@ const onLogin = async () => {
 <template>
   <div class="space-y-6">
     <h1 class="text-2xl font-semibold text-center">
-      Acesse sua conta 2
+      Acesse sua conta 3
     </h1>
     <form method="post" @submit.prevent="onLogin">
       <FormField
